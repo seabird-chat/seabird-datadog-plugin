45db048f9ddfcf7d21c98d807dfacff5195fe3f4
Jared Ledvina <jared@techsmix.net>
2021-03-12 13:42:30 -0500

    Metric!
    Signed-off-by: Jared Ledvina <jared@techsmix.net>

--- client.go
+++ client.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"errors"
+	"fmt"
 	"log"
 
 	"github.com/DataDog/datadog-go/statsd"
@@ -12,7 +13,6 @@ import (
 // SeabirdClient is a basic client for seabird
 type SeabirdClient struct {
 	*seabird.Client
-
 	datadogClient *statsd.Client
 }
 
@@ -40,6 +40,16 @@ func (c *SeabirdClient) close() error {
 
 func (c *SeabirdClient) messageCallback(event *pb.MessageEvent) {
 	log.Printf("Processing event: %s %s", event.Source, event.Text)
+	channelTag := fmt.Sprintf("channel:%s", event.Source.ChannelId)
+	displayNameTag := fmt.Sprintf("display_name:%s", event.Source.User.DisplayName)
+	userTag := fmt.Sprintf("user:%s", event.Source.User.Id)
+	tags := []string{
+		channelTag,
+		displayNameTag,
+		userTag,
+	}
+	// TODO: This assumes max of 1 message per second per user.
+	c.datadogClient.Count("seabird.message", 1, tags, 1)
 }
 
 // Run runs
@@ -53,7 +63,7 @@ func (c *SeabirdClient) Run() error {
 	for event := range events.C {
 		switch v := event.GetInner().(type) {
 		case *pb.Event_Message:
-			c.messageCallback(v.Message)
+			go c.messageCallback(v.Message)
 		}
 	}
 	return errors.New("event stream closed")
